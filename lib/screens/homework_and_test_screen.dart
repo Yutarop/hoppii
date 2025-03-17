16f1837f4b4b7e78acb5145dfafb90df2e1a64b5
Yutaro <katoyutaro1122@icloud.com>
2025-03-17 21:38:03 +0900

    add a new folder that contains screens

---
+++ lib/screens/homework_and_test_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeworkAndTestScreen extends StatelessWidget {
+  static const String id = 'homework_and_test_screen';
+  const HomeworkAndTestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/schedule_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ScheduleScreen extends StatelessWidget {
+  static const String id = 'schedule_screen';
+  const ScheduleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
