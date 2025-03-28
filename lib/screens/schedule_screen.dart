4b7719d7accaaeac0085ed5ec1cd94a086ed8460
Yutaro <katoyutaro1122@icloud.com>
2025-03-28 16:49:35 +0900

    update UI of the schedule screen

--- lib/screens/schedule_screen.dart
+++ lib/screens/schedule_screen.dart
@@ -3,14 +3,49 @@ import 'package:flutter/material.dart';
 class ScheduleScreen extends StatelessWidget {
   static const String id = 'schedule_screen';
   static const int index = 0;
-  const ScheduleScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Work',
-      ),
-    );
+    return scheduleReturn();
   }
 }
+
+Material scheduleReturn() {
+  List<String> weekList = ['','月', '火', '水', '木', '金', '土',];
+  List<Widget> childrenCol = [];
+  for(int k = 0; k < 7; k++){
+    List<Widget> childrenRow = [];
+    for(int i = 0; i < 7; i++){
+      childrenRow.add(Expanded(
+        flex: i == 0 ? 1 : 3,
+        child: Container(
+          child: Center(child: k == 0 ?  Text('${weekList[i]}'): Text('i $i, k $k')),
+          decoration: BoxDecoration(
+            border: i == 0 && k != 0 ? Border.symmetric(
+              vertical: BorderSide.none,
+              horizontal: BorderSide(color: Colors.grey, width: 1.2,),
+            ) : Border.symmetric(
+              vertical: BorderSide.none,
+              horizontal: BorderSide(color: Colors.grey, width: 1.2, ),
+            ),
+          ),
+        ),
+      ),);
+    }
+    childrenCol.add(Expanded(
+      flex: k == 0 ? 1 : 3,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: childrenRow,
+      )
+    ));
+  }
+  return Material(
+    color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: childrenCol,
+    ),
+  );
+}
+
